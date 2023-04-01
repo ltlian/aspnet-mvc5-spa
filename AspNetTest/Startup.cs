3d063fc3acd94767b5492f5c8f6def6952392e4f
lars <larstomaslian@gmail.com>
2023-04-01 17:25:15 +0200

    Initialize .Net Framework 4.8 MVC project

---
+++ AspNetTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetTest.Startup))]
+namespace AspNetTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
