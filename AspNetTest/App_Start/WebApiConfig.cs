c54cb0570c8b4cfe8b5dce390d23c82e88907aed
lars <larstomaslian@gmail.com>
2023-04-02 20:18:58 +0200

    Enable CORS during dev

--- AspNetTest/App_Start/WebApiConfig.cs
+++ AspNetTest/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace AspNetTest
 {
@@ -13,6 +14,18 @@ namespace AspNetTest
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            if (Environment.GetEnvironmentVariable("DEV_ENVIRONMENT") == "1")
+            {
+                // Enable all CORS for dev mode to allow requests from app running on different port
+                // https://learn.microsoft.com/en-us/aspnet/web-api/overview/security/enabling-cross-origin-requests-in-web-api
+                var cors = new EnableCorsAttribute(origins: "*", headers: "*", methods: "*")
+                {
+                    SupportsCredentials = true
+                };
+
+                config.EnableCors(cors);
+            }
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
